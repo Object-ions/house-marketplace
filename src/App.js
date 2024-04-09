c734f8d21a42b8c04efdaa64a7ae49b01d3a2808
Object-ions <moshikoatia@gmail.com>
2024-04-08 19:01:54 -0700

    Add Listing to the routes

--- src/App.js
+++ src/App.js
@@ -11,21 +11,27 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
+
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Explore />}></Route>
-          <Route path="/offers" element={<Offers />}></Route>
-          <Route path="/category/:categoryName" element={<Category />}></Route>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path="/sign-in" element={<SignIn />}></Route>
-          <Route path="/sign-up" element={<SignUp />}></Route>
-          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-          <Route path="/create-listing" element={<CreateListing />}></Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
